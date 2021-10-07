aabed69c18443a22687d785562a491981b07ecb3
Mert PC <mahmutkaantumen@gmail.com>
2021-10-07 14:50:22 +0300

    graph resize.

--- src/screens/Graph.tsx
+++ src/screens/Graph.tsx
@@ -29,20 +29,17 @@ function Graph(props: dataType) {
 
   function dragOver(e: any){
     e.preventDefault();
-    if(e.clientX > 475 || e.clientY > 475 ) return
-    console.log(e.clientX,e.clientY)
-    const leftSide = Math.floor(e.clientX * 100 / 475)
-    const bottomSide = Math.floor(100 - (e.clientY * 100 / 475))
+    if(e.clientX > 375 || e.clientY > 375 ) return
+    const leftSide = Math.floor(e.clientX * 100 / 375)
+    const bottomSide = Math.floor(100 - (e.clientY * 100 / 375))
     changeRow({...currentData, vision: leftSide, ability: bottomSide}, currentData.id)
   }
 
   function drop(e: any) {
     e.preventDefault();
-    if(e.clientX > 475 || e.clientY > 475 ) return
-    console.log(e.clientX,e.clientY)
-    const leftSide = Math.floor(e.clientX * 100 / 475)
-    const bottomSide = Math.floor(100 - (e.clientY * 100 / 475))
-
+    if(e.clientX > 375 || e.clientY > 375 ) return
+    const leftSide = Math.floor(e.clientX * 100 / 375)
+    const bottomSide = Math.floor(100 - (e.clientY * 100 / 375))
     changeRow({...currentData, vision: leftSide, ability: bottomSide}, currentData.id)
 
   }
@@ -54,18 +51,19 @@ function Graph(props: dataType) {
 
   return (
     <div>
-      <div style={{width: 500, height: 500, border: "2px solid black"}} onDragOver={e=> {dragOver(e)}} onDrop={e => {drop(e)}} onDragEnd={e => {drop(e)}} >
-        <div className="Graph-Labels" style={{border: "2px solid lightgrey", position:"absolute", top: 250, width:495}}/>
-        <div className="Graph-Labels" style={{border: "2px solid lightgrey", position:"absolute", top: 250, width:495, transform:"rotate(90deg)"}}/>
-        <div className="Graph-Labels" style={{marginLeft:75}}>Challengers</div>
-        <div className="Graph-Labels" style={{marginLeft:350}}>Leaders</div>
-        <div className="Graph-Labels" style={{marginLeft:75,top:470}}>Niche Players</div>
-        <div className="Graph-Labels" style={{marginLeft:350,top:470}}>Visionaries</div>
-        <div style={{transform:"rotate(270deg)", position:"absolute", marginLeft:450,top:420}}>Ability to Execute -&#62; </div>
-        <div style={{position:"absolute",marginLeft:300,top:510}}>&#60;- Completeness of Vision </div>
+      <div style={{width: 400, height: 400, border: "2px solid black"}} onDragOver={e=> {dragOver(e)}} onDrop={e => {drop(e)}} onDragEnd={e => {drop(e)}} >
+        <div className="Graph-Labels" style={{border: "2px solid lightgrey", position:"absolute", top: 200, width:395}}/>
+        <div className="Graph-Labels" style={{border: "2px solid lightgrey", position:"absolute", top: 200, width:395, transform:"rotate(90deg)"}}/>
+        <div className="Graph-Labels" style={{marginLeft:35}}>Challengers</div>
+        <div className="Graph-Labels" style={{marginLeft:250}}>Leaders</div>
+        <div className="Graph-Labels" style={{marginLeft:35,top:370}}>Niche Players</div>
+        <div className="Graph-Labels" style={{marginLeft:250,top:370}}>Visionaries</div>
+        <div style={{transform:"rotate(270deg)", position:"absolute", marginLeft:350,top:320}}>Ability to Execute -&#62; </div>
+        <div style={{position:"absolute",marginLeft:200,top:410}}>&#60;- Completeness of Vision </div>
         {props.data.map((row: objectType) => {
-          const leftSide = Math.floor(row.vision * 475 / 100)
-          const bottomSide = Math.floor(475 - (row.ability * 475 / 100))
+          const leftSide = Math.floor(row.vision * 375 / 100)
+          const bottomSide = Math.floor(375 - (row.ability * 375 / 100))
+
           return(
               <span key={row.id} className="dot" style={{ height: 25, width: 25, backgroundColor: "#3878A2", borderRadius: "50%",
                 display: "inline-block", position: "absolute", left: leftSide, top:bottomSide }}
